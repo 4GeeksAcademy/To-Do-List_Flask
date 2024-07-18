835115963f5941ba06afbd2fedf7dd157395b4c2
Marcel Rivero <126285355+Marci25@users.noreply.github.com>
2024-07-18 19:35:31 +0000

    First commit

---
+++ src/app.py
@@ -0,0 +1,31 @@
+from flask import Flask,jsonify,request
+
+
+app = Flask(__name__)
+
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    json_text = jsonify(todos)
+    return json_text
+
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    data = request.json
+    todos.append(data)
+    print("incoming request with the following body", data)
+    print('todos', todos)
+    return todos, 200
+
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print('This is the position to delete', position)
+    return todos
+
+some_data = {"name": "Bobby", "lastname": "Rixer"}
+todos = [{"label": "My first task", "done": False}]
+
+if __name__ =='__main__':
+    app.run(host='0.0.0.0', port=3245, debug=True)
